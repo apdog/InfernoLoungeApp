125f177faf32814573594560158d2ceaa9317509
apdog <alex.puhow@gmail.com>
2023-07-05 22:33:35 +0300

    добавлен activity_news_item.xml

---
+++ app/src/main/java/com/example/infernolounge/presentation/activity/NewsItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.infernolounge.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.infernolounge.R
+
+class NewsItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_item)
+    }
+}
